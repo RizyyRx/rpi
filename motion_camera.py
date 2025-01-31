96946505a448f0382b9115127640b0fafe696822
riz <rizwankendo@gmail.com>
2025-01-31 14:27:32 +0000

    Captured photos are sent to an endpoint to be displayed in realtime

--- motion_camera.py
+++ motion_camera.py
@@ -3,6 +3,8 @@ import time
 import os
 from ratelimit import limits
 from uuid import uuid4
+from threading import Thread
+import requests
 
 DEVICE_ID = "raspi_cam_1"
 STORAGE_LOCATION = "motion_captures"
@@ -15,6 +17,22 @@ channel = 27
 
 GPIO.setup(channel,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
+def upload_picture(filename):
+    name = filename.split("/")[-1]
+
+    url = "http://172.30.0.60:5000/api/motion/capture"
+
+    payload = {}
+    files=[
+    ('file',(name, open(filename,'rb'),'image/jpeg'))
+    ]
+    headers = {
+    'X-Authorization': 'Bearer c528f646-6fdd-47c0-8d45-616a7de64e17',
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+    print(response.text)
 
 @limits(calls=1, period=1)
 def click_picture():
@@ -22,6 +40,9 @@ def click_picture():
     uuid = str(uuid4())
     os.system('libcamera-still -o '+STORAGE_LOCATION+ "/" +uuid+'.jpg --timeout 1000 --verbose 0')
     print("done")
+    t = Thread(target = upload_picture, args=(STORAGE_LOCATION+ "/" +uuid+'.jpg',))
+    t.start()
+
 try:
     while True:
         if GPIO.input(channel):
